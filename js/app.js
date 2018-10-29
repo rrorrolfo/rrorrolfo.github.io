e00c686dac822de077a31e6bac90ec71be19e5b8
Rodolfo Perez <rodolfobeste@gmail.com>
2018-10-29 23:09:46 +0200

    Added responsiveness for tablet and desktop version

--- js/app.js
+++ js/app.js
@@ -8,6 +8,12 @@ $menu.click( () => {
     $nav.toggle("slide", {direction: "right"});
 })
 
+window.addEventListener("resize", () => {
+    if (window.screen.width > 768) {
+        $nav.show;
+    }
+})
+
 // CHANGE PATH OF SVGs WHEN HOVER
 
 const nav = document.querySelector("nav");
